102404127712a693918c124a4b6196c858912d1a
travisneilturner <travisneilturner@gmail.com>
2019-10-30 07:26:14 -0700

    Done!

--- PracticeInterview2.js
+++ PracticeInterview2.js
@@ -1,77 +1,165 @@
 let Graph = function() {
-	this.adj = {}
-	this.parent = {}
-	this.discovered = new Set()
-	this.order = []
+  this.adj = {}
+  this.discovered = new Set()
+  this.parent = {}
+  this.processed = new Set()
+  this.topologicalOrder = []
+  this.vertices = new Set()
 }
 
-Graph.prototype.topologicalOrder = function() {
-	return this.order.reverse()
+Graph.prototype.classifyEdge = function(source, dest) {
+  if (this.parent[dest] === source) {
+    return 'TREE'
+  } else if(this.discovered.has(dest) && !this.processed.has(dest)) {
+    return 'BACK'
+  } else {
+    return 'CROSS|FORWARD'
+  }
 }
+
+Graph.prototype.findPath = function(path, start, end) {
+  if (start === end || !this.parent[end]) {
+    path.push(start)
+    return path
+  } else {
+    path.push(end)
+    return this.findPath(path, start, this.parent[end])
+  }
+}
+
+Graph.prototype.isOrderableOver = function(chars) {
+  let nodes = new Set()
+
+  for (let c of chars) {
+    if (!this.vertices.has(c)) {
+      return false
+    }
+  }
+
+  this.vertices.forEach((v) => {
+    nodes.add(v)
+  })
+
+  // remove all nodes with incoming edges
+  Object.keys(this.adj).forEach((source) => {
+    for (let dest of this.adj[source]) {
+      nodes.delete(dest) // dest has an incoming edge
+    }
+  })
+
+  // Test to make sure remaining nodes with no incoming edges have outgoing edges
+  nodes.forEach((node) => {
+    if (this.adj[node].length === 0) {
+      return false
+    }
+  })
+
+  if (nodes.size > 1) {
+    return false
+  }
+
+  return true
+} 
 
 function alphabeticalOrdering(words) {
-	let graph = new Graph()
+  let graph = new Graph()
+  let chars = new Set()
 
-	if(words.length === 0) {
+	if (words.length === 0) {
 		throw new Error('Word list cannot be empty')
 	}
 
-	if(words.length === 1) {
+	if (words.length === 1) {
 		if (words[0].length != 1) {
 			throw new Error('Single word input with multiple characters -- insufficient information to provide alphabet')
 		} else {
 			return [words[0].charAt(0)]
 		}
-	}
+  }
 
 	// discover edges in graph
 	for (let i = 0; i < words.length-1;  i++) {
 		let chars1 = words[i].split('')
-		let chars2 = words[i+1].split('')
+    let chars2 = words[i+1].split('')
+    
+    chars1.forEach((char) => {
+      if(!chars.has(char)) {
+        chars.add(char)
+      }
+    })
+
+    chars2.forEach((char) => {
+      if(!chars.has(char)) {
+        chars.add(char)
+      }
+    })
 
 		let i1 = 0, i2 = 0
-		while(chars1[i1] === chars2[i2]) {
-			if(i1 === chars1.length-1 || i2 === chars2.length-1) {
+		while (chars1[i1] === chars2[i2]) {
+			if (i1 === chars1.length-1 || i2 === chars2.length-1) {
 				break
 			}
 			i1++
 			i2++
+    }
+    
+    if (chars1[i1] != chars2[i2]) {
+      if (!graph.adj[chars1[i1]]) {
+        graph.vertices.add(chars1[i1])
+        graph.adj[chars1[i1]] = []
+      }
+      if (!graph.vertices.has(chars2[i2])) {
+        graph.vertices.add(chars2[i2])
+      }
+			graph.adj[chars1[i1]].push(chars2[i2])
 		}
+  }
 
-		if(!graph.adj[chars1[i1]]) {
-			graph.adj[chars1[i1]] = []
-			graph.adj[chars1[i1]].push(chars2[i2])
-		} else {
-			graph.adj[chars1[i1]].push(chars2[i2])
-		}
-	}
+  if (Object.keys(graph.adj).length === 0) {
+    throw new Error('Invalid input -- character set cardinality of 1')
+  }
+
+  if (!graph.isOrderableOver(chars)) {
+    throw new Error('Input data is not orderable')
+  }
 
 	// topological sort
-	for(let node of Object.keys(graph.adj)) {
-		if(!graph.discovered.has(node)) {
+	for (let node of Object.keys(graph.adj)) {
+		if (!graph.discovered.has(node)) {
 			dfs(graph, node)
 		}
 	}
 	
-	return graph.topologicalOrder()
+	return graph.topologicalOrder.reverse()
+}
+
+function processEdge(graph, source, dest) {
+  let edgeCategory = graph.classifyEdge(source, dest)
+  if (edgeCategory === 'BACK') {
+    console.log('[WARN] Back edge detected, data set contains cycle: ', [dest, ...graph.findPath([], dest, source)].reverse().join(','))
+  }
 }
 
 function dfs(graph, node) {
-
 	graph.discovered.add(node)
 
-	if(!graph.adj[node]) {
-		graph.order.push(node)
+	if (!graph.adj[node]) {
+		graph.topologicalOrder.push(node)
 		return
 	}
 
-	for(let adj of graph.adj[node]) {
-		if(!graph.discovered.has(adj)) {
+	for (let adj of graph.adj[node]) {
+		if (!graph.discovered.has(adj)) {
+      graph.parent[adj] = node
+      processEdge(graph, node, adj)
 			dfs(graph, adj)
-		}
+		} else {
+      processEdge(graph, node, adj)
+    } 
 	}
 
-	graph.order.push(node)
+  graph.processed.add(node)
+	graph.topologicalOrder.push(node)
 }
 
 
@@ -87,22 +175,43 @@ words = ['a']
 expected = ['a']
 assertEqual(alphabeticalOrdering(words), expected, 'Test one character')
 
+words = []
+expected = ['a']
+let test = () => alphabeticalOrdering(words)
+assertThrowsError(test, 'Test empty input')
+
+words = ['a', 'a', 'a']
+expected = ['a']
+test = () => alphabeticalOrdering(words)
+assertThrowsError(test, 'Test one repeated character')
+
+words = ['a', 'b', 'c']
+expected = ['a', 'b', 'c']
+assertEqual(alphabeticalOrdering(words), expected, 'Test one character sequence')
+
 words = ['aaaaaa']
-expected = ['d', 'a', 'b', 'c', 'z']
-const test = () => alphabeticalOrdering(words)
+test = () => alphabeticalOrdering(words)
 assertThrowsError(test, 'Test multiple characters => insufficient data (single word)')
 
-words = ['zbcd', 'zbca', 'zbcb', 'zbcc', 'zbca']
-expected = ['d', 'a', 'b', 'c']
-assertEqual(alphabeticalOrdering(words), expected, 'Test ordering cycle present')
+words = ['zbcd', 'zbca', 'zbcb', 'zbcc', 'zbca', 'zbci']
+test = () => alphabeticalOrdering(words)
+assertThrowsError(test, 'Test input not orderable 1')
+
+words = ['ac', 'bc', 'bd']
+test = () => alphabeticalOrdering(words)
+assertThrowsError(test, 'Test input not orderable 2')
+
+words = ['abc', 'ac', 'acb']
+test = () => alphabeticalOrdering(words)
+assertThrowsError(test, 'Test input not orderable 3')
 
 words = ['a', 'b', 'by']
-expected = ['a', 'b']
-assertEqual(alphabeticalOrdering(words), expected, 'Test ordering invalid input (ambiguous extra character)')
+test = () => alphabeticalOrdering(words)
+assertThrowsError(test, 'Test input not orderable 4 (ambiguous extra character)')
 
 words = ['a', 'b', 'by', 'bv', 'ca', 'cy', 'cz', 'cr']
 expected = ['a', 'y', 'z', 'r', 'v', 'b', 'c']
-assertEqual(alphabeticalOrdering(words), expected, 'Test ordering invalid input (ambiguous extra character)')
+assertEqual(alphabeticalOrdering(words), expected, 'Test input is orderable but has non-total order')
 
 function assertEqual(a, b, desc) {
   if (JSON.stringify(a) === JSON.stringify(b)) {
